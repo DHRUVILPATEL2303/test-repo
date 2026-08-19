c58d84eb9d1f6e5bd068c6830271984a4ee6c062
Dhruvil Patel <142326138+DHRUVILPATEL2303@users.noreply.github.com>
2026-08-19 08:18:27 +0530

    Update main.go

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 	go func() {
 		for {
 			
-				log.Println("[INFO]teri gand bc sorry sorry..")
+				log.Println("[INFO]teri gand bc sorry sorry ssss..")
 			time.Sleep(3 * time.Second)
 		}
 	}()
